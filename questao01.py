f78725aa703908151dc657bb5cddeaf784e71f75
Jesse <neogi.xlr@hotmail.com>
2026-01-10 19:43:15 -0300

    Primeiro commit

---
+++ questao01.py
@@ -0,0 +1,32 @@
+#Programa de Saudação
+print("Olá, mundo!")
+
+#Calculadora de Soma
+numero1 = 12
+numero2 = 14
+
+soma = numero1 + numero2
+
+print("A soma é:", soma)
+
+
+#Calculadora de Volume
+comprimento = 12  # cm
+largura = 14      # cm
+altura = 20       # cm
+
+volume = comprimento * largura * altura
+
+print("O volume da caixa é:", volume, "cm³")
+
+#Calculadora de Preço Total
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+preco_total = preco_unitario * quantidade
+
+print("Produto:", nome_produto)
+print("Preço unitário: R$", preco_unitario)
+print("Quantidade:", quantidade)
+print("Preço total da compra: R$", preco_total)
